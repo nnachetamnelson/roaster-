bf5d86069a0c36a04b3fec4338d53cc0c97d53f0
nnachetamnelson <70458095+nnachetamnelson@users.noreply.github.com>
2023-01-13 22:52:27 +0100

    Update Roste.py

--- Roste.py
+++ Roste.py
@@ -1,5 +1,4 @@
-
-import datetime
+ import datetime
 
 def generate_roster(employees):
     roster = {}
@@ -9,9 +8,14 @@ def generate_roster(employees):
         days_worked = 0
         while days_worked < 7:
             if current_date.weekday() not in [5, 6]: # if not saturday or sunday
-                if current_date.hour < 15 and current_date.hour >= 19: # if before 3 PM and after 7 PM
-                    roster[employee].append(current_date)
-                    days_worked += 1
+                current_time = datetime.time(7, 0) # 7 AM
+                current_datetime = datetime.datetime.combine(current_date, current_time)
+                if current_datetime.hour < 15: # if before 3 PM
+                    current_time = datetime.time(19, 0) # 7 PM
+                    current_datetime = datetime.datetime.combine(current_date, current_time)
+                    if current_datetime.hour >= 19:
+                        roster[employee].append(current_date)
+                        days_worked += 1
                 if days_worked % 7 == 6: # if employee has worked 6 days in a row
                     current_date += datetime.timedelta(days=1) # give employee a day off
             current_date += datetime.timedelta(days=1)
